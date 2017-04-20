6908553c356d387013a9aebfeed9f01912e12b88
Isaac Stone <isaac@nimblecollective.com>
2017-04-20 15:14:37 -0700

    added ticket auth option

--- js/connect.js
+++ js/connect.js
@@ -1,4 +1,5 @@
 var _password;
+var _ticket;
 
 function wsConnect(connectionPub, connectionSub, url, realm) {
 	connectionPub.onopen = function (session) {
@@ -40,19 +41,21 @@ function wsConnectAnonymous(url, realm)
 }
 
 function onchallenge (session, method, extra) {
-        if (method === "wampcra") {
+    if (method === "wampcra") {
 			var secret = _password;
 			if ("keylen" in extra) {
 				secret = autobahn.auth_cra.derive_key(_password, extra.salt, extra.iterations, extra.keylen);
 			}
 			
-            return autobahn.auth_cra.sign(secret, extra.challenge);
-        }
+      return autobahn.auth_cra.sign(secret, extra.challenge);
+    } else if (method === "ticket") {
+      return _ticket;
     }
+}
     
 function wsConnectWampcra(url, realm, id)
 {
-	_password = id.password;
+	  _password = id.password;
 	
     var connectionPub = new autobahn.Connection({
         url: url,
@@ -66,6 +69,29 @@ function wsConnectWampcra(url, realm, id)
         url: url,
         realm: realm,
         authmethods: ["wampcra"],
+        authid: id.user,
+        onchallenge: onchallenge
+    });
+    
+    wsConnect(connectionPub, connectionSub, url, realm);
+}
+
+function wsConnectTicket(url, realm, id)
+{
+    _ticket = id.ticket;
+
+    var connectionPub = new autobahn.Connection({
+        url: url,
+        realm: realm,
+        authmethods: ["ticket"],
+        authid: id.user,
+        onchallenge: onchallenge
+    });
+
+    var connectionSub = new autobahn.Connection({
+        url: url,
+        realm: realm,
+        authmethods: ["ticket"],
         authid: id.user,
         onchallenge: onchallenge
     });

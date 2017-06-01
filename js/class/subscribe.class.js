dc56b02dd07b247769b0da507e0eb2d5c402ab24
Isaac Stone <isaac@nimblecollective.com>
2017-06-01 11:08:40 -0700

    fixed subscribe to handle kwargs

--- js/class/subscribe.class.js
+++ js/class/subscribe.class.js
@@ -4,15 +4,23 @@ function Subscribe(topic) {
     var ptr;
 
     this.callback = function (args, kwargs, details) {
-        var data = JSON.stringify(args);
+      console.log({ args, kwargs, details });
+        var dataObj = args.length ? args : kwargs;
+        var data = JSON.stringify(dataObj, undefined, 1);
         var date = getCurrentTime();
-        _dataList[_dataIndex] = JSON.stringify(args, undefined, 4);
+        var idx = _dataIndex;
+        _dataList[idx] = JSON.stringify(dataObj, undefined, 4);
 
-        var html = '<tr onclick="displayData('+_dataIndex+', \''+this.topic+'\', \''+date+'\', \''+details.publisher+'\');" style="background-color: rgba('+color[0]+', '+color[1]+', '+color[2]+', 0.4);">'
-        html += '<td>'+this.topic+'</td><td class="flow-col-date">'+date+'</td>';
-        html += '<td class="flow-col-publisher">'+details.publisher+'</td><td class="flow-col-data"><span>'+data.slice(0, 34)+' ...</span></td></tr>';
+        var tr = $('<tr>', { class: 'flow-tr' });
+        tr.css('background-color', 'rgba('+color[0]+', '+color[1]+', '+color[2]+', 0.4)');
+        tr.click(function() { displayData(idx, topic, date, details.publisher); });
+        
+        $('<td>', { class: 'flow-col-topic', text: topic }).appendTo(tr);
+        $('<td>', { class: 'flow-col-date', text: date }).appendTo(tr);
+        $('<td>', { class: 'flow-col-publisher', text: '' + details.publisher }).appendTo(tr);
+        $('<td>', { class: 'flow-col-data', text: data.substr(0, 120) }).appendTo(tr);
 
-        $('#listener').prepend(html);
+        $('#listener').prepend(tr);
         
         _dataIndex++;
     };
--- js/rpc.js
+++ js/rpc.js
@@ -8,24 +8,32 @@ function rpcCallPopup(uri) {
 }
 
 function rpcCall() {
-    var args = [];
+    var args = [],
+        kwargs = {};
 
     for(var i=1 ; i<=5 ; i++) {
-        var value = $('#rpc-arg'+i).val();
-
-        if (value != "") {
+        var value = $('#rpc-arg'+i).val(),
+            match = value.match(/: /);
+        if (match) {
+            var k = value.substring(0, match.index).trim(),
+                v = value.substring(match.index + 2).trim();
+            kwargs[k] = v;
+        } else if (value != "") {
             args.push(value);
         }
     }
+    console && console.log && console.log({ "args": args, "kwargs": kwargs });
 
-    wsPub.call(_callURI, args).then(
+    wsPub.call(_callURI, args, kwargs).then(
         function (result) {
             $('#callRPC').modal('hide');
             alert('Call result : '+result);
+            console && console.log && console.log({ "result": result });
         },
         function (error) {
-			$('#callRPC').modal('hide');
-			displayError(error);
+            $('#callRPC').modal('hide');
+            console && console.log && console.log({ "error": error });
+            displayError(error);
         }
     );
 }
@@ -60,7 +68,7 @@ function newRpc(details) {
     var html = '<tr id="rpc-'+details.id+'"><td>'+details.uri+'</td>';
     html += '<td class="rpc-col-created">'+details.created +'</td>';
     html += '<td class="rpc-col-callees"><span id="callees-list-'+details.id+'" class="sessions-details" onclick="getSessionsDetails(\''+details.uri+'\', \''+details.id+'\', \'rpc\');">'
-    				+JSON.stringify(details.callees)+'</span></td>';
+                    +JSON.stringify(details.callees)+'</span></td>';
     html += '<td class="rpc-col-action"><button class="btn btn-info btn-compact" onclick="rpcCallPopup(\''+details.uri+'\');">Call</button></td>';
     html += '</tr>';
 
@@ -96,11 +104,11 @@ function getAllRpc() {
     );
 
     wsPub.subscribe('wamp.registration.on_create', onRegister).then(
-    	function (res) {},
-    	displayError
+        function (res) {},
+        displayError
     );
     wsPub.subscribe('wamp.registration.on_delete', onUnregister).then(
-    	function (res) {},
-    	displayError
+        function (res) {},
+        displayError
     );
 }
